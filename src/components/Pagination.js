3303cb61a770d03c6d77054cd29ce069b5e879e1
hanhan0303 <105223867+hanhan0303@users.noreply.github.com>
2025-12-29 21:11:06 +0800

    註解刪除

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -46,52 +46,6 @@ export default function Pagination({ pagination, changePage }) {
           </li>
         </ul>
       </nav>
-      {/* <nav aria-label="Page navigation example">
-        <ul className="pagination">
-          <li className="page-item">
-            <a
-              href="/"
-              aria-label="Previous"
-              className={`page-link ${pagination.has_pre ? '' : 'disabled'}`}
-              onClick={(e) => {
-                e.preventDefault();
-                changePage(pagination.current_page - 1);
-              }}
-            >
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          {[...new Array(pagination.total_pages)].map((_, i) => (
-            <li className="page-item" key={`${i}_page`}>
-              <a
-                href="/"
-                className={`page-link ${
-                  i + 1 === pagination.current_page && 'active'
-                }`}
-                onClick={(e) => {
-                  e.preventDefault();
-                  changePage(i + 1);
-                }}
-              >
-                {i + 1}
-              </a>
-            </li>
-          ))}
-          <li className="page-item">
-            <a
-              className={`page-link ${pagination.has_next ? '' : 'disabled'}`}
-              onClick={(e) => {
-                e.preventDefault();
-                changePage(pagination.current_page + 1);
-              }}
-              href="/"
-              aria-label="Next"
-            >
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav> */}
     </>
   );
 }
